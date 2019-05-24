adaac6c0a237d52084f871781e6f08b301dcefd9
marusiakulyk <33452986+marusiakulyk@users.noreply.github.com>
2019-05-24 04:47:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import Linear_regression
 
 
 def process_data(matrix, y):
-    coef = Linear_regression.lin(matrix, y)
-    res = coef[0] + Linear_regression.vector_matrix_multip(coef[1:], y)
-    return res
+    x_ = [[1]+i[1:] for i in matrix]
+    vector = [i[0] for i in matrix]
+    coef = Linear_regression.lin(x_, vector)
+    res = [coef[0]] + Linear_regression.vector_matrix_multip(coef[1:], y)
+    res_num = 0
+    for i in res: res_num += i
+    return res_num
